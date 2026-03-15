a6c5a8746ab91adf837ff1b259368e9d2a7bd692
Abhishek Jain <jainabhishek356@gmail.com>
2026-03-15 17:20:51 +0530

    fix: import Cell from recharts to fix ReferenceError in RoleBreakdown

--- src/components/charts/RoleBreakdown.js
+++ src/components/charts/RoleBreakdown.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { useMemo } from 'react';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 export default function RoleBreakdown({ budgets }) {
   const data = useMemo(() => {
